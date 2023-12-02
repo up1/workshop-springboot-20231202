f05bcb94c69e130f3cc639def86acd3d66eff007
up1 <somkiat.p@gmail.com>
2023-12-02 15:15:14 +0700

    Add logging

--- day01/src/main/java/com/example/day01/UserController.java
+++ day01/src/main/java/com/example/day01/UserController.java
@@ -10,17 +10,20 @@ public class UserController {
 
     @GetMapping("/") // GET /users/
     public List<UserResponse> getAll() {
+        System.out.println("Get all");
         return null;
     }
 
     @GetMapping("/{id}") // GET /users/1
     public UserResponse getUserById(@PathVariable int id) {
+        System.out.println("GET /users/1");
         return null;
     }
 
     @GetMapping("")  // GET /users?id=1
     public UserResponse
         getUserByIdWithParams(@RequestParam(defaultValue = "0") int id) {
+        System.out.println("GET /users?id=1");
         return null;
     }
 
