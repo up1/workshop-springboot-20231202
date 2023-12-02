fb33babaafe096a4f9550845c0cf25c9f6ef0d0e
up1 <somkiat.p@gmail.com>
2023-12-02 14:02:44 +0700

    Add second test case

--- day01/src/test/java/com/example/day01/HelloControllerTest.java
+++ day01/src/test/java/com/example/day01/HelloControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +22,13 @@ public class HelloControllerTest {
         HelloResponse result
                 = restTemplate.getForObject("/hello", HelloResponse.class);
         assertEquals("Hello Spring Boot", result.getMessage());
+    }
+
+    @Test
+    void sayHello2() {
+        ResponseEntity<HelloResponse> result
+                = restTemplate.getForEntity("/hello2", HelloResponse.class);
+        assertEquals(201, result.getStatusCode().value());
+        assertEquals("Hello Spring Boot", result.getBody().getMessage());
     }
 }
