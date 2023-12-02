70d3f6b86209a499950afdd28c6196c74b4be5d3
up1 <somkiat.p@gmail.com>
2023-12-02 11:34:10 +0700

    Custom response code

--- day01/src/main/java/com/example/day01/HelloController.java
+++ day01/src/main/java/com/example/day01/HelloController.java
@@ -1,10 +1,21 @@
 package com.example.day01;
 
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/hello2")
+    public ResponseEntity<HelloResponse> sayHello2() {
+        HelloResponse response = new HelloResponse();
+        response.setMessage("Hello Spring Boot");
+        ResponseEntity<HelloResponse> r
+                = new ResponseEntity<>(response, HttpStatusCode.valueOf(201));
+        return  r;
+    }
 
     @GetMapping("/hello")
     public HelloResponse sayHello() {
--- day01/src/main/java/com/example/day01/HelloResponse.java
+++ day01/src/main/java/com/example/day01/HelloResponse.java
@@ -1,13 +1,13 @@
 package com.example.day01;
 
-public class HelloResponse {
-    private String message;
+public class HelloResponse{
+	private String message;
 
-    public String getMessage() {
-        return message;
-    }
+	public void setMessage(String message){
+		this.message = message;
+	}
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+	public String getMessage(){
+		return message;
+	}
 }
