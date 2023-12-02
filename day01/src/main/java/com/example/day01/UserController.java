9598598f555c58477bb6c4eb5e0238ef6586d396
up1 <somkiat.p@gmail.com>
2023-12-02 15:09:56 +0700

    Add more routes

--- day01/src/main/java/com/example/day01/UserController.java
+++ day01/src/main/java/com/example/day01/UserController.java
@@ -1,9 +1,6 @@
 package com.example.day01;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,13 +8,19 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("")
+    @GetMapping("/") // GET /users/
     public List<UserResponse> getAll() {
         return null;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") // GET /users/1
     public UserResponse getUserById(@PathVariable int id) {
+        return null;
+    }
+
+    @GetMapping("")  // GET /users?id=1
+    public UserResponse
+        getUserByIdWithParams(@RequestParam(defaultValue = "0") int id) {
         return null;
     }
 
