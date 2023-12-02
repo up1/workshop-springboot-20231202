b7ab2b2da5e3988bbffb681878128e29ba4a736f
up1 <somkiat.p@gmail.com>
2023-12-02 15:49:22 +0700

    Fix

--- day01/src/main/java/com/example/day01/UserCommandController.java
+++ day01/src/main/java/com/example/day01/UserCommandController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserCommandController {
 
-    @PostMapping("/users")
+    @PostMapping(path="/users")
     public UserResponse createNewUser(
             @RequestBody UserRequest userRequest) {
         System.out.println(userRequest);
--- day01/src/main/java/com/example/day01/UserResponse.java
+++ day01/src/main/java/com/example/day01/UserResponse.java
@@ -1,4 +1,32 @@
 package com.example.day01;
 
 public class UserResponse {
+
+    private String last_name;
+    private String first_name;
+    private int age;
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
