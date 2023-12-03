91eeba309c488af72d395a3aaea625f81ffad439
up1 <somkiat.p@gmail.com>
2023-12-03 16:27:32 +0700

    Change start

--- day01/src/main/java/com/example/day01/NewMain.java
+++ day01/src/main/java/com/example/day01/NewMain.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
-public class Day01Application {
+public class NewMain {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Day01Application.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewMain.class, args);
+    }
 
 }
