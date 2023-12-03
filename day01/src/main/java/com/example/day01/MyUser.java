25ee6bc3827eb031dbadc2f3c1df5b1d7071cd45
up1 <somkiat.p@gmail.com>
2023-12-03 11:58:02 +0700

    Add repository layer

---
+++ day01/src/main/java/com/example/day01/MyUser.java
@@ -0,0 +1,50 @@
+package com.example.day01;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_users")
+public class MyUser {
+    @Id
+    private int id;
+    @Column(name = "fname")
+    private String firstName;
+    @Column(name = "lname")
+    private String lastName;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
