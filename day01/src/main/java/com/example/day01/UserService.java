7198d14e9480858aba2890f24d86f45f677519ee
up1 <somkiat.p@gmail.com>
2023-12-03 10:42:22 +0700

    Add logic to check user

--- day01/src/main/java/com/example/day01/UserService.java
+++ day01/src/main/java/com/example/day01/UserService.java
@@ -5,8 +5,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    private boolean isExistingUserInDb(String firstName) {
+        // TODO :: check from database
+        return "dummy".equals(firstName);
+    }
+
     public UserResponse createNewUser(UserRequest userRequest) {
         // Process ?
+        if(isExistingUserInDb(userRequest.getFirst_name())) {
+            throw new RuntimeException("User existed");
+        }
+
+        // Ok
+        // TODO :: Create a new User into database
+
         // Return response
         UserResponse response = new UserResponse();
         response.setFirst_name(userRequest.getFirst_name());
@@ -15,5 +27,7 @@ public class UserService {
         response.setId(100);
         return response;
     }
+
+
 
 }
